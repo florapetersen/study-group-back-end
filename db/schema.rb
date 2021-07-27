3ce1c174c07b55f807e7a1a1911204df7f621680
florapetersen <floramarina@comcast.net>
2021-07-26 20:26:10 -0700

    updated routes and schema to include courses and schools

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_21_221806) do
+ActiveRecord::Schema.define(version: 2021_07_27_032444) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.string "subject"
+    t.string "instructor"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "schools", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
