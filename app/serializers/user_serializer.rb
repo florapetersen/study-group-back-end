eb9c117512ac6e63f356f6e0db9654e6a845a9f5
florapetersen <floramarina@comcast.net>
2021-07-28 16:25:45 -0700

    comment methods to access serializer data

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,3 +2,8 @@ class UserSerializer
   include JSONAPI::Serializer
   attributes :id, :email, :created_at
 end
+
+#to access serializer data for single record:
+#UserSerializer.new(resource).serializable_hash[:data][:attributes]
+#to access serializer data for multiple records:
+#UserSerializer.new(resource).serializable_hash[:data].map{|data| data[:attributes]}
