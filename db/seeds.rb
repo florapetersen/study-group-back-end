7e789b965173e5d31ca395d9a2314f8c81525e18
florapetersen <floramarina@comcast.net>
2021-07-31 20:01:13 -0700

    seeded database with sample courses

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+course1 = Course.find_or_create_by(name: 'Biology')
+course2 = Course.find_or_create_by(name: 'Chemistry')
